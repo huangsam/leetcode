a360c595ab3bff598dc37f927f02d168353572b1
Samuel Huang <samhua@cisco.com>
2017-07-06 14:57:57 -0700

    Optimize hammingDistance solution

--- algorithms/python/hammingDistance.py
+++ algorithms/python/hammingDistance.py
@@ -1,8 +1,5 @@
 # https://leetcode.com/problems/hamming-distance/
 class Solution(object):
-    def getBinary(self, val):
-        return bin(val)[2:][::-1]
-
     def hammingDistance(self, x, y):
         """
         :type x: int
@@ -10,21 +7,8 @@ class Solution(object):
         :rtype: int
         """
         distance = 0
-        x = self.getBinary(x)
-        y = self.getBinary(y)
-        x_len, y_len = len(x), len(y)
-        x_ind, y_ind = 0, 0
-        while x_ind < x_len and y_ind < y_len:
-            if x[x_ind] != y[y_ind]:
+        xor_result = bin(x ^ y)[2:]
+        for bit in xor_result:
+            if bit == '1':
                 distance += 1
-            x_ind += 1
-            y_ind += 1
-        while x_ind < x_len:
-            if x[x_ind] == '1':
-                distance += 1
-            x_ind += 1
-        while y_ind < y_len:
-            if y[y_ind] == '1':
-                distance += 1
-            y_ind += 1
         return distance
