cbd998c8513e8adce4abd3e7f5f3d27245f5a527
Samuel Huang <samhua@cisco.com>
2017-07-14 15:46:22 -0700

    Add deleteNode solution

---
+++ algorithms/python/deleteNode.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/delete-node-in-a-bst/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def deleteNode(self, root, key):
+        """
+        :type root: TreeNode
+        :type key: int
+        :rtype: TreeNode
+        """
+        if root is None:
+            return None
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+            minNode = self.findMin(root.right)
+            root.val = minNode.val
+            root.right = self.deleteNode(root.right, root.val)
+        return root
+
+    def findMin(self, node):
+        while node.left:
+            node = node.left
+        return node
+
