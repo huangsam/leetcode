7fb2311ef9ac1141cd33ca76a16cea8f7ebb6651
Samuel Huang <samhuang91@gmail.com>
2017-07-16 23:01:39 -0700

    Add findDisappearedNumbers solution

---
+++ algorithms/python/findDisappearedNumbers.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        result = []
+        n_len = len(nums)
+        for i in range(n_len):
+            num = abs(nums[i])
+            if nums[num - 1] > 0:
+                nums[num - 1] *= -1
+        n_len = len(nums)
+        for i in range(n_len):
+            if nums[i] > 0:
+                result.append(i + 1)
+        return result
