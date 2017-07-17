d948f6ea5174bb2b0b4e1b30e51e2593f7f7e909
Samuel Huang <samhua@cisco.com>
2017-07-17 12:17:54 -0700

    Add singleNumber solution

---
+++ algorithms/python/singleNumber.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/single-number/
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        single = 0
+        for num in nums:
+            single ^= num
+        return single
