cebc5419dae42aa69b6be59fd2b2c45a640e79fc
Samuel Huang <samhua@cisco.com>
2017-08-08 08:50:17 -0700

    Add isPowerOfFour solution

---
+++ algorithms/python/isPowerOfFour.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/power-of-four/
+class Solution(object):
+    def isPowerOfFour(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 0:
+            return False
+        while num % 4 == 0:
+            num = num / 4
+        return num == 1
