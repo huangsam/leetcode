efc62fa4f7e72e9a2ad827d5a49b8e5973dcd63c
Samuel Huang <samhua@cisco.com>
2017-08-12 12:03:40 -0700

    Add isBalanced solution

---
+++ algorithms/python/isBalanced.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/balanced-binary-tree/
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def nodeHeight(self, root):
+        if root is None:
+            return 0
+        left_height = self.nodeHeight(root.left)
+        if left_height == -1:
+            return -1
+        right_height = self.nodeHeight(root.right)
+        if right_height == -1:
+            return -1
+        if abs(left_height - right_height) > 1:
+            return -1
+        return max(left_height, right_height) + 1
+
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return self.nodeHeight(root) != -1
