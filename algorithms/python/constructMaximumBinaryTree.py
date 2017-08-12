86cb14939a562d4c7f2566074f8e450ccc72f232
Samuel Huang <samhua@cisco.com>
2017-08-12 11:19:58 -0700

    Add constructMaximumBinaryTree solution

---
+++ algorithms/python/constructMaximumBinaryTree.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/maximum-binary-tree/
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def constructMaximumBinaryTree(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if len(nums) == 0:
+            return None
+
+        max_num = None
+        max_ind = None
+        for ind, num in enumerate(nums):
+            if max_num is None and max_ind is None:
+                max_num = num
+                max_ind = ind
+            elif max_num < num:
+                max_num = num
+                max_ind = ind
+        root = TreeNode(max_num)
+        root.left = self.constructMaximumBinaryTree(nums[:max_ind])
+        root.right = self.constructMaximumBinaryTree(nums[(max_ind + 1):])
+        return root
