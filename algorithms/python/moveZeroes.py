63f0396c8acf91e943b8e7e2d97b7a9ae0dd6764
Samuel Huang <samhua@cisco.com>
2017-08-18 13:25:34 -0700

    Add moveZeroes solution

---
+++ algorithms/python/moveZeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        non_zero_found = 0
+        for i in range(0, len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[non_zero_found] = nums[non_zero_found], nums[i]
+                non_zero_found += 1
