4a0fee24ad9f61be4e192fe1a71b7304cbf78cff
Samuel Huang <samhuang91@gmail.com>
2018-09-05 09:14:26 -0700

    Add uniquePaths solutions

---
+++ algorithms/python/uniquePaths.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/unique-paths/description/
+class Solution:
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        # initialize memo array
+        memo = [[0] * n for i in range(m)]
+
+        # go through each row, m-1 --> 0
+        for r in range(m - 1, -1, -1):
+            # go through each col, n-1 --> 0
+            for c in range(n - 1, -1, -1):
+                # base case -> at the end
+                if r == m - 1 and c == n - 1:
+                    memo[r][c] = 1
+                # can go down
+                if r + 1 < m:
+                    memo[r][c] += memo[r + 1][c]
+                # can go right
+                if c + 1 < n:
+                    memo[r][c] += memo[r][c + 1]
+
+        # solve original problem --> starting point = (0,0)
+        return memo[0][0]
---
+++ algorithms/python/uniquePaths2.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/unique-paths-ii/description/
+class Solution:
+
+    def uniquePathsWithObstacles(self, m, n):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+
+        # initialize memo array
+        memo = [[0] * n for i in range(m)]
+
+        # go through each row, m-1 --> 0
+        for r in range(m - 1, -1, -1):
+            # go through each col, n-1 --> 0
+            for c in range(n - 1, -1, -1):
+                # base case -> at the end
+                if r == m - 1 and c == n - 1:
+                    memo[r][c] = 1
+                # can go down
+                if r + 1 < m:
+                    memo[r][c] += memo[r + 1][c]
+                # can go right
+                if c + 1 < n:
+                    memo[r][c] += memo[r][c + 1]
+
+        # solve original problem --> starting point = (0,0)
+        return memo[0][0]
