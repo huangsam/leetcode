3782ff3b60f3496e98030bf6fce70850301bb0f6
Samuel Huang <samhuang91@gmail.com>
2018-09-08 15:26:04 -0700

    Create sortColors.py

---
+++ algorithms/python/sortColors.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/sort-colors/description/
+class Solution:
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        l, m, h = 0, 0, len(nums)-1
+        while m <= h:
+            if nums[m] == 0:
+                nums[l], nums[m] = nums[m], nums[l]
+                l += 1
+                m += 1
+            elif nums[m] == 1:
+                m += 1
+            elif nums[m] == 2:
+                nums[m], nums[h] = nums[h], nums[m]
+                h -= 1
