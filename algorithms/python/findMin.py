863970d7bfe43ff5c687787dfb0ddc429a4d95f5
Samuel Huang <samhuang91@gmail.com>
2018-09-08 11:57:47 -0700

    Update findMin.py

--- algorithms/python/findMin.py
+++ algorithms/python/findMin.py
@@ -14,3 +14,19 @@ class Solution:
         if mid > last:
             return self.findMin(nums[m:h+1])
         return self.findMin(nums[l:m+1])
+
+    def findMinIterative(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l, h = 0, len(nums)-1
+        while h - l > 2:
+            m = (l + h) // 2
+            mid = nums[m]
+            last = nums[h]
+            if mid > last:
+                l, h = m, h
+            else:
+                l, h = l, m
+        return min(nums[l:h+1])
