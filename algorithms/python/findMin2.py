40921725683c621866e246b6e44ad6525bd96c38
Samuel Huang <samhuang91@gmail.com>
2018-09-08 12:16:35 -0700

    Create findMin2.py

---
+++ algorithms/python/findMin2.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/description/
+class Solution:
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l, h = 0, len(nums) - 1
+        while l < h:
+            m = (l + h) >> 1
+            if nums[m] > nums[h]:
+                l = m + 1
+            elif nums[m] < nums[h]:
+                h = m
+            else:
+                h = h - 1
+        return nums[l]
