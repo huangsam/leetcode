b0aeb754e0fd6ddb32cac7913289cc2789dc5cdf
Samuel Huang <samhuang91@gmail.com>
2018-09-08 22:04:37 -0700

    Create maxArea.py

---
+++ algorithms/python/maxArea.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        i, j = 0, len(height) - 1
+        water = 0
+        while i < j:
+            water = max(water, (j - i) * min(height[i], height[j]))
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+        return water
