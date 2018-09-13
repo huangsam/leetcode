0343cf1e9bd08a6eef227f5f9cdbe1bd60af909e
Samuel Huang <samhuang91@gmail.com>
2018-09-12 22:39:48 -0700

    Modify binarySearch in searchRange

--- algorithms/python/searchRange.py
+++ algorithms/python/searchRange.py
@@ -1,8 +1,31 @@
 # https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
 class Solution:
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l, h = 0, len(nums)
+        found = self.binarySearch(nums, target, 0, len(nums) - 1)
+        if found == -1:
+            return [-1, -1]
+        left, right = found, found
+        while True:
+            result = self.binarySearch(nums, target, 0, left - 1)
+            if result == -1:
+                break
+            left = result
+        while True:
+            result = self.binarySearch(nums, target, right + 1, len(nums) - 1)
+            if result == -1:
+                break
+            right = result
+        return [left, right]
+
     def binarySearch(self, nums, target, lo, hi):
         found = -1
-        while lo < hi:
+        while lo <= hi:
             mid = (lo + hi) // 2
             if nums[mid] == target:
                 found = mid
@@ -10,27 +33,5 @@ class Solution:
             elif nums[mid] < target:
                 lo, hi = mid + 1, hi
             else:
-                lo, hi = lo, mid
+                lo, hi = lo, mid - 1
         return found
-
-    def searchRange(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        found = self.binarySearch(nums, target, 0, len(nums))
-        if found == -1:
-            return [-1, -1]
-        left, right = found, found
-        while True:
-            result = self.binarySearch(nums, target, 0, left)
-            if result == -1 or result == left:
-                break
-            left = result
-        while True:
-            result = self.binarySearch(nums, target, right, len(nums))
-            if result == -1 or result == right:
-                break
-            right = result
-        return [left, right]
