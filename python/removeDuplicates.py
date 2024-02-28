ed1dcdea1133964e2a87291a18fd31ddd96a946f
Samuel Huang <samhuang91@gmail.com>
2024-02-27 23:10:32 -0800

    Update removeDuplicates.py

--- python/removeDuplicates.py
+++ python/removeDuplicates.py
@@ -1,15 +1,24 @@
 # https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 class Solution:
-    def removeDuplicates(self, nums):
+    def removeDuplicates(self, nums: List[int]) -> int:
         """
-        :type nums: List[int]
-        :rtype: int
+        Remove duplicates.
+
+        Approach:
+        - 0 or 1 is the base case
+        - Numbers are already sorted in increasing order
+        - Numbers are either duplicates or not
+        - Iterate through numbers until a non-duplicate is found
+        - Move non-duplicate over to next unique position
+        - Return unique position + 1, since the offset starts at 0
         """
-        nums_found = 0
-        cur_num = None
-        for num in nums:
-            if cur_num is None or cur_num != num:
-                cur_num = num
-                nums[nums_found] = cur_num
-                nums_found += 1
-        return nums_found
+        if len(nums) <= 1:
+            return len(nums)
+        n_cur = 0
+        n_uniq = 0
+        while n_cur < len(nums):
+            if nums[n_cur] != nums[n_uniq]:
+                n_uniq += 1
+                nums[n_uniq] = nums[n_cur]
+            n_cur += 1
+        return n_uniq + 1
