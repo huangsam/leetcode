53ded61d7708b9e1261e2fe1e5a01f2766ed8e4f
Samuel Huang <samhuang91@gmail.com>
2024-02-29 23:58:07 -0800

    Update maxProfit.py

--- python/maxProfit.py
+++ python/maxProfit.py
@@ -1,20 +1,33 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-class Solution(object):
-    def maxProfit(self, prices):
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         """
-        :type prices: List[int]
-        :rtype: int
+        You want to maximize your profit by choosing a single
+        day to buy one stock and choosing a different day in
+        the future to sell that stock.
+
+        Return the maximum profit you can achieve from
+        this transaction. If you cannot achieve any
+        profit, return 0.
+
+        prices = [7,1,5,3,6,4]
+        output = 5
+
+        Approach:
+        - Look for smallest value for future days
+        - If the current price is <= prev, update smallest
+        - If the current price is > prev, calculate profit
+        - Return the best positive profit you can find
         """
-        alen = len(prices)
-        i = 0
-        small = 0
+        if len(prices) < 1:
+            raise ValueError("Got non-empty list of prices")
         max_profit = 0
-        while i < alen:
-            if prices[i] < prices[small]:
-                small = i
-                continue
-            current_profit = prices[i] - prices[small]
-            if max_profit < current_profit:
-                max_profit = current_profit
-            i += 1
+        min_price_so_far = prices[0]
+        for price in prices:
+            if price <= min_price_so_far:
+                min_price_so_far = price
+            else:
+                current_profit = price - min_price_so_far
+                if current_profit > max_profit:
+                    max_profit = current_profit
         return max_profit
