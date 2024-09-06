647fbe3a21241f43df9081e57eed9f5af13e86cf
Samuel Huang <samhuang91@gmail.com>
2024-09-05 21:56:55 -0700

    Create addMinimum.py

---
+++ python/addMinimum.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/minimum-additions-to-make-valid-string/
+class Solution:
+    def addMinimum(self, word: str) -> int:
+        """
+        a -> 2
+        b -> 2
+        c -> 2
+        ab -> 1
+        bc -> 1
+        abc -> 0
+        """
+        curr_seq = []
+        num_letters = 0
+
+        for ch in word:
+            # If we find a character of earlier/same ascii value
+            # then we tally our score and reset the sequence
+            if len(curr_seq) > 0 and ch <= curr_seq[-1]:
+                num_letters += 3 - len(curr_seq)
+                curr_seq = []
+
+            # Always add to the current sequence so that we
+            # know how many characters are valid in the current
+            # sequence
+            curr_seq.append(ch)
+
+        # It is possible that the last sequence is not cleared at the
+        # end of the main loop, so we handle it here
+        return num_letters + (3 - len(curr_seq))
