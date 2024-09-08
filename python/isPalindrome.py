5a7fe2dfc15a325fd7a2d7b3ce5358f5ce9cc087
Samuel Huang <samhuang91@gmail.com>
2024-09-08 00:54:18 -0700

    Update isPalindrome.py
    Change docstring and add link to top of file

--- python/isPalindrome.py
+++ python/isPalindrome.py
@@ -1,8 +1,9 @@
+# https://leetcode.com/problems/valid-palindrome/
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         """
-        Collect all letters, converting them to lowercase as we go. Then
-        see if the reverse iteration is the same as the forward iteration.
+        Collect all letters and numbers, converting them to lowercase as we go.
+        Then see if the reverse iteration is the same as the forward iteration.
         """
         char_list = [ch.lower() for ch in s if ch.isalnum()]
         return all(
