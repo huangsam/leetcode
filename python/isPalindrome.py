f44695819bcae095471f36f58c3fa914202d5ee0
Samuel Huang <samhuang91@gmail.com>
2024-09-08 00:47:20 -0700

    Create isPalindrome.py

---
+++ python/isPalindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        """
+        Collect all letters, converting them to lowercase as we go. Then
+        see if the reverse iteration is the same as the forward iteration.
+        """
+        char_list = [ch.lower() for ch in s if ch.isalnum()]
+        return all(
+            char_list[i] == char_list[len(char_list) - i - 1]
+            for i in range(len(char_list))
+        )
