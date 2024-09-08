95f9b983ffd68fb80cdc85a9ac21279d616fec31
Samuel Huang <samhuang91@gmail.com>
2024-09-08 01:18:07 -0700

    Add comments for grep in urls.sh

--- urls.sh
+++ urls.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 # Match for LeetCode URLs in all solution folders
+
+# Here are the command options explained:
+#   -o = Print matching parts of matching line
+#   -a = Process binary file as text
+#   -h = Supress prefixing of filename
+#   -r = Read all files under each directory
 grep -o -a -h -r "https://leetcode.com.*" {java,python}
