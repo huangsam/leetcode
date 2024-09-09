d3523e4708a063f86c8779f8ed8d5031e8904a9d
Samuel Huang <samhuang91@gmail.com>
2024-09-08 22:28:33 -0700

    Add alternative approach to solution

--- python/countBits.py
+++ python/countBits.py
@@ -3,6 +3,10 @@ class Solution:
         """
         Get the binary representation for each number i from
         0..n and literally count the number of 1s in each
-        string...
+        string.
+
+        The alternative is to xor/and the powers of 2 to the
+        each number i and tallying up the score. That would
+        be a O(n lg n) algo in that case.
         """
         return [bin(i)[2:].count('1') for i in range(n + 1)]
