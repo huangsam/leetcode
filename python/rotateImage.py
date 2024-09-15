a52227344a89c361c6b34d98f4164b217ea91092
Samuel Huang <samhuang91@gmail.com>
2024-09-15 15:48:11 -0700

    Improve readability of rotateImage.py

--- python/rotateImage.py
+++ python/rotateImage.py
@@ -1,21 +1,42 @@
 # https://leetcode.com/problems/rotate-image/
 class Solution:
-    def rotate(self, matrix):
+    def rotate(self, matrix: List[List[int]]) -> None:
         """
-        :type matrix: List[List[int]]
-        :rtype: void Do not return anything, modify matrix in-place instead.
+        Do not return anything, modify matrix in-place instead.
+
+        Observation for 3x3:
+            starts at 0,0 -> 0,2 -> 2,2 -> 2,0 (size 3)
+            ends at 1,1 due to size 1 (size 1)
+
+        Observation for 4x4:
+            starts at 0,0 -> 0,3 -> 3,3 -> 3,0 (size 4)
+            starts at 1,1 -> 1,2 -> 2,2 -> 2,1 (size 2)
+            ends at 2,2 due to size 0 (size 0)
         """
-        m_len = len(matrix)
-        # squares
-        for x in range(m_len // 2):
-            # elements
-            for y in range(x, m_len - x - 1):
-                north = matrix[x][y]
+        size, start = len(matrix), 0
+        while size > 1:
+            # avoid shuffling the corners twice
+            for i in range(size - 1):
+                end = start + size - 1
+                forward, backward = start + i, end - i
+
+                # store top
+                top = matrix[start][forward]
+
                 # top <- left
-                matrix[x][y] = matrix[m_len - y - 1][x]
+                matrix[start][forward] = matrix[backward][start]
+
                 # left <- bottom
-                matrix[m_len - y - 1][x] = matrix[m_len - x - 1][m_len - y - 1]
+                matrix[backward][start] = matrix[end][backward]
+
                 # bottom <- right
-                matrix[m_len - x - 1][m_len - y - 1] = matrix[y][m_len - x - 1]
+                matrix[end][backward] = matrix[forward][end]
+
                 # right <- top
-                matrix[y][m_len - x - 1] = north
+                matrix[forward][end] = top
+
+            # decrease size by 2, since N-S-E-W all reduce by 1
+            size -= 2
+
+            # move starting position to (i + 1, j + 1)
+            start += 1
