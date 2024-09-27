84dc841acb001bc6131d77d9943cb1acab8b8366
Samuel Huang <samhuang91@gmail.com>
2024-09-27 08:43:00 -0700

    Create reverseBetween.py

---
+++ python/reverseBetween.py
@@ -0,0 +1,57 @@
+# https://leetcode.com/problems/reverse-linked-list-ii/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        """
+        We reverse a subset of the singly linked list from
+        left <= right. Anything less than left and anything greater
+        than right keeps the same ordering.
+
+        The simplest way is to disconnect the left <= right subset
+        from the linked list, reverse it and reconnect it back to
+        the list afterwards. Assuming that left > 1 or right still
+        has at least one node afterwards.
+
+        Keep in mind that left can equal right. So in that case, nothing
+        needs to be done as it were.
+        """
+        if not head:
+            return None
+        elif left == right:
+            return head
+
+        # Find point for breaking left (sentinel or real)
+        break_left: ListNode = ListNode(val=-1000, next=head)
+        for _ in range(left - 1):
+            break_left = break_left.next
+
+        # Find point for breaking right (null or real)
+        break_right: ListNode | None = head
+        for _ in range(right):
+            break_right = break_right.next
+
+        # Reverse between left < right, since we know that
+        # left != right by this point. By the end, we have
+        # current pointing to the new head. Tail points to
+        # the starting point
+        current = break_left.next.next
+        prev = break_left.next
+        tail = prev
+        for _ in range(right - left):
+            nxt = current.next
+            current.next = prev
+            prev = current
+            current = nxt
+
+        # Reconnect broken left if applicable
+        break_left.next = prev
+
+        # Reconnect broken right if applicable
+        tail.next = break_right
+
+        return head if left > 1 else prev
