926318d6431681100706a075c36d0685006d4755
Samuel Huang <samhuang91@gmail.com>
2024-09-27 12:24:47 -0700

    Update deleteDuplicates.py
    Simplify logic to modify linked list in-place

--- python/deleteDuplicates.py
+++ python/deleteDuplicates.py
@@ -12,34 +12,23 @@ class Solution:
         val_freq: Dict[Any, int] = defaultdict(int)
         all_nodes = []
 
-        current = head
-        while current:
-            val_freq[current.val] += 1
-            all_nodes.append(current)
-            current = current.next
+        curr = head
+        while curr:
+            val_freq[curr.val] += 1
+            all_nodes.append(curr)
+            curr = curr.next
 
         # Add dummy to handle 1 node and 2..n node cases
-        unique_nodes = [ListNode()]
+        dummy = prev = ListNode(next=None)
 
-        # Add nodes which do not have duplicates
-        unique_nodes.extend(n for n in all_nodes if val_freq.get(n.val) == 1)
+        curr = head
+        while curr:
+            temp = curr.next
+            if val_freq.get(curr.val) == 1:
+                prev.next = curr
+                prev = curr
+                curr.next = None
+            curr = temp
 
-        # Abort processing if all nodes are duplicates of each other
-        if len(unique_nodes) == 1:
-            return None
+        return dummy.next
 
-        # We know that the linked list is already sorted, so
-        # need to pre-sorting before linking things
-        curr, prev = 1, 0
-        while curr < len(unique_nodes):
-            curr_node = unique_nodes[curr]
-            prev_node = unique_nodes[prev]
-
-            prev_node.next = curr_node
-            prev_node = curr_node
-            curr_node.next = None
-
-            curr, prev = curr + 1, prev + 1
-
-        # Return min node as it is unique, non-dupe, sorted
-        return unique_nodes[1]
