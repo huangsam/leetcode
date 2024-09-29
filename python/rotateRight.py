0e8d9d2ff4ee512e7ba83d1dbe73c4284841fe55
Samuel Huang <samhuang91@gmail.com>
2024-09-28 17:42:29 -0700

    Create rotateRight.py

---
+++ python/rotateRight.py
@@ -0,0 +1,49 @@
+# https://leetcode.com/problems/rotate-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        """
+        One possible approach is as follows:
+
+        - Get length of list in one pass
+        - Compute (k % length) and create a pointer at this distance
+        - Set left node as head, set right node as head + distance
+        - Iterate until next pointer of distance pointer is null (i.e. tail)
+        - Unlink left.next from the target node
+        - Link tail.next to the original head
+        - Return the target node
+        """
+        if not head:
+            return None
+
+        curr, size = head, 0
+        while curr:
+            curr = curr.next
+            size += 1
+
+        window = k % size
+        if window == 0:
+            return head
+
+        left, right = head, head
+        for _ in range(window):
+            right = right.next
+
+        while right.next:
+            left = left.next
+            right = right.next
+
+        # Collect target pointer and unlink left node from it
+        target = left.next
+        left.next = None
+
+        # Link tail to original head
+        right.next = head
+
+        # Return target pointer
+        return target
