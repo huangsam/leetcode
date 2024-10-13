ba5c3146a7783cc1e8493af5dfa0e3cd507bcbd8
Samuel Huang <samhuang91@gmail.com>
2024-10-12 20:00:30 -0700

    Reformat logic once more

--- java/MergeKLists.java
+++ java/MergeKLists.java
@@ -31,8 +31,8 @@ public class MergeKLists {
         }
         return (list1.val < list2.val)
             ? new ListNode(
-            list1.val, mergeTwoLists(list1.next, list2))
+                list1.val, mergeTwoLists(list1.next, list2))
             : new ListNode(
-            list2.val, mergeTwoLists(list1, list2.next));
+                list2.val, mergeTwoLists(list1, list2.next));
     }
 }
