078af49d0b1dbb1486a440716ed0c716c1877d4b
Samuel Huang <samhuang91@gmail.com>
2024-10-23 10:09:06 -0700

    Create flattenBinaryTree.py

---
+++ python/flattenBinaryTree.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+
+from container.binary_tree import TreeNode
+
+
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if root is None:
+            return
+
+        if root.left is None:
+            self.flatten(root.right)
+            return
+
+        self.flatten(root.left)
+        self.flatten(root.right)
+
+        left, right = root.left, root.right
+
+        # We no longer want any nodes on left side
+        root.left = None
+
+        # We want all nodes on the right side
+        root.right = left
+
+        # Traverse until we hit the tail of the right side
+        curr = root.right
+        while curr.right is not None:
+            curr = curr.right
+
+        # Connect the left.tail with the right.head
+        curr.right = right
+
+        return root
