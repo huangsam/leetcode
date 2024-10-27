303df8ce5bee4853a1c9decf10ba11747eab9c96
Samuel Huang <samhuang91@gmail.com>
2024-10-26 21:54:42 -0700

    Add JavaDoc to MinimumDifferenceBst

--- java/MinimumDifferenceBST.java
+++ java/MinimumDifferenceBST.java
@@ -3,8 +3,18 @@
 import container.TreeNode;
 
 public class MinimumDifferenceBST {
-    private TreeNode visited;
+    private TreeNode last;
 
+    /**
+     * We assume that the input is a binary search tree with at least two
+     * nodes. Based on this, we can always assume there is a previous node
+     * and a current node. As we perform inorder traversal, we can check
+     * for the node that's being visited and see if there was a previously
+     * visited node. If that node exists, we subtract the difference between
+     * them, otherwise we return positive infinity. Then at the root node, we
+     * collect the results from left and right sides, and get the minimum of
+     * all results.
+     */
     public int getMinimumDifference(TreeNode root) {
         if (root == null) {
             return Integer.MAX_VALUE;
@@ -12,9 +22,9 @@ public class MinimumDifferenceBST {
 
         int leftResult = getMinimumDifference(root.left);
 
-        int result = (visited != null) ? root.val - visited.val : Integer.MAX_VALUE;
+        int result = (last != null) ? root.val - last.val : Integer.MAX_VALUE;
 
-        visited = root;
+        last = root;
 
         int rightResult = getMinimumDifference(root.right);
 
