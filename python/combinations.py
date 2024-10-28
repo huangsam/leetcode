0cfacb8ce89aa3df68de31e77046de49c87e7aa8
Samuel Huang <samhuang91@gmail.com>
2024-10-27 19:17:25 -0700

    Implement combinations.py

---
+++ python/combinations.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/combinations/
+
+from typing import List
+
+
+class Solution:
+    def __init__(self) -> None:
+        self.result: List[List[int]] = []
+
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        self.helper(n, k, [])
+        return self.result
+
+    def helper(self, n: int, k: int, nums: List[int]) -> None:
+        if k == 0:
+            self.result.append(nums)
+            return
+
+        start = nums[-1] + 1 if nums else 1
+
+        # Traverse start to n with enough buffer for k items
+        for i in range(start, n - k + 2):
+            self.helper(n, k - 1, nums + [i])
