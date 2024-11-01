8884dc3127b8a5e79e2ae98ccceac1d71abce647
Samuel Huang <samhuang91@gmail.com>
2024-11-01 12:12:35 -0700

    Create minSubArray.py
    Keep in mind that the right += 1 is inclusive, so no need for (right - left + 1) as usual. Pays to know whether an index is inclusive or exclusive

---
+++ python/minSubArray.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/minimum-size-subarray-sum/
+
+from typing import List
+
+
+class Solution:
+    MAX_VALUE = 2 ** 32
+
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        """
+        Slide left and right as you will. Right increases until
+        the current_sum beats or matches the target. Left increases
+        until the current_sum is lower than the target. Continue
+        updating min_length in all cases. We return the minimum possible
+        length at the end of the iteration.
+        """
+        left, right = 0, 0
+
+        min_length = Solution.MAX_VALUE
+
+        current_sum = 0
+
+        while right < len(nums):
+            current_sum += nums[right]
+            right += 1
+            while current_sum >= target:
+                min_length = self.newMin(min_length, left, right)
+                current_sum -= nums[left]
+                left += 1
+
+        if min_length == Solution.MAX_VALUE:
+            return 0
+
+        return min_length
+
+    def newMin(self, current: int, left: int, right: int) -> int:
+        return min(current, right - left)
