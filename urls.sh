aa96c4140a900c54b44114d8e1c77f33b1e897d7
Samuel Huang <samhuang91@gmail.com>
2024-11-03 09:59:03 -0800

    Remove duplicate links and refine comments

--- urls.sh
+++ urls.sh
@@ -2,9 +2,17 @@
 
 # Match for LeetCode URLs in all solution folders
 
-# Here are the command options explained:
-#   -o = Print matching parts of matching line
-#   -a = Process binary file as text
-#   -h = Supress prefixing of filename
-#   -r = Read all files under each directory
-grep -o -a -h -r "https://leetcode.com.*" {java,python}
+<<'eof'
+We use grep to find all LeetCode URLs in the various folders.
+
+Here are the grep options explained:
+    -o = Print matching parts of matching line
+    -a = Process binary file as text
+    -h = Supress prefixing of filename
+    -r = Read all files under each directory
+
+We then apply sort and uniq to enforce unique links. That is because
+uniq only detects duplicates on adjacent lines.
+eof
+
+grep -o -a -h -r "https://leetcode.com.*" {java,python} | sort | uniq
