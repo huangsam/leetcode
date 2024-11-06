2b11cc06c12f3428e3dfe962bb89dd471d06755d
Samuel Huang <samhuang91@gmail.com>
2024-11-06 01:39:35 -0800

    Implement BinaryTreeRightSide.java

---
+++ java/BinaryTreeRightSide.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/binary-tree-right-side-view/
+
+import container.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class BinaryTreeRightSide {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        Deque<DepthNode> deque = new ArrayDeque<>();
+        deque.addFirst(new DepthNode(1, root));
+
+        while (!deque.isEmpty()) { // BFS traversal
+            DepthNode current = deque.removeLast();
+            int currDepth = current.depth;
+            TreeNode currNode = current.node;
+            if (currNode == null) {
+                continue;
+            }
+
+            // Collect rightmost value from the new row
+            if (currDepth > result.size()) {
+                result.add(currNode.val);
+            }
+
+            deque.addFirst(new DepthNode(currDepth + 1, currNode.right));
+            deque.addFirst(new DepthNode(currDepth + 1, currNode.left));
+        }
+
+        return result;
+    }
+
+    private static record DepthNode(int depth, TreeNode node) {}
+}
