2b8960a524ef6f598d6156ed45acaf4a2b06ca75
Samuel Huang <samhuang91@gmail.com>
2024-11-06 03:40:46 -0800

    Improve space and time efficiency

--- java/LongestConsecutive.java
+++ java/LongestConsecutive.java
@@ -3,15 +3,8 @@
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.Stack;
 
 public class LongestConsecutive {
-    private Stack<int[]> intervals;
-
-    public LongestConsecutive() {
-        intervals = new Stack<>();
-    }
-
     public int longestConsecutive(int[] nums) {
         int result = 0;
 
@@ -19,44 +12,24 @@ public class LongestConsecutive {
         Arrays.stream(nums).forEach(allNums::add);
 
         for (int num : nums) {
-            if (isInInterval(num)) {
-                continue;
-            }
-            int lower = num, higher = num;
-            while (allNums.contains(lower - 1)) {
-                lower--;
-            }
-            while (allNums.contains(higher + 1)) {
-                higher++;
-            }
-            if (bestLength() < higher - lower + 1) {
-                updateInterval(lower, higher);
+            if (allNums.contains(num)) {
+                int currentLength = 1;
+                int lower = num - 1, higher = num + 1;
+
+                while (allNums.contains(lower)) {
+                    currentLength++;
+                    allNums.remove(lower--); // avoid duplicate traversals
+                }
+
+                while (allNums.contains(higher)) {
+                    currentLength++;
+                    allNums.remove(higher++); // avoid duplicate traversals
+                }
+
+                result = Math.max(result, currentLength);
             }
         }
-        return bestLength();
-    }
 
-    /** Avoid duplicate interval searches */
-    private boolean isInInterval(int num) {
-        for (int[] interval : intervals) {
-            if (interval[0] <= num && num <= interval[1]) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /** Get best length found so far */
-    private int bestLength() {
-        if (intervals.isEmpty()) {
-            return 0;
-        }
-        int[] interval = intervals.peek();
-        return interval[1] - interval[0] + 1;
-    }
-
-    /** Update interval so far */
-    private void updateInterval(int left, int right) {
-        intervals.push(new int[]{left, right});
+        return result;
     }
 }
