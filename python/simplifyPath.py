048b940a0fd5e094588c5a1b7270cd9ffe2eb8f5
Samuel Huang <samhuang91@gmail.com>
2024-11-06 20:41:18 -0800

    Optimize performance

--- python/simplifyPath.py
+++ python/simplifyPath.py
@@ -7,17 +7,16 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         """
         Assume that no spaces exist in folder names and file names.
-        Then we can replace '/' with ' ' - so that 1 or more spaces
-        can be split conveniently. Then for all the incoming tokens
-        that are parsed, we check if it's a '.', '..' or something
+        Then we can split by '/' to identify all incoming tokens.
+        For each token, we check if it's a '.', '..' or something
         else. '.' is a no-op as it points to the current directory.
-        A '..' is the previous directory, so we pop out the current
-        directory. Everything else is a new name.
+        A '..' is the previous directory, so we pop it out.
+        Everything else is a new name so we add it.
         """
         path_stack: List[str] = []
 
-        for token in path.replace("/", " ").split():
-            if token == '.':
+        for token in path.split('/'):
+            if token in {'', '.'}:
                 continue
             elif token == '..':
                 if len(path_stack) > 0:
