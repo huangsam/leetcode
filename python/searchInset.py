7992cd94b988880a9c2a0a35313002a9baadede7
Samuel Huang <samhuang91@gmail.com>
2024-11-07 07:53:57 -0800

    Implement searchInset.py

---
+++ python/searchInset.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/search-insert-position/
+
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        """
+        Similar to regular binary search. But the main difference
+        in this case is that we are looking for an insert position
+        between two numbers (for middle case) or an insert position
+        on either side. For the left side, checking for midpoint set
+        to zero is sufficient. For the right side, returning the lo
+        pointer is sufficient since it should be only one number
+        higher than the highest number possible.
+        """
+        lo = 0
+        hi = len(nums) - 1
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            if nums[mid] >= target:
+                # Note that the list values are distinct
+                if mid == 0 or nums[mid - 1] < target:
+                    return mid
+            if nums[mid] > target:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return lo
