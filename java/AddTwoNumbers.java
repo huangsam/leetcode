bce348c737c7809a4725a95f84173c016c53125b
Samuel Huang <samhuang91@gmail.com>
2024-11-07 09:26:37 -0800

    Improve readability of AddTwoNumbers.java

--- java/AddTwoNumbers.java
+++ java/AddTwoNumbers.java
@@ -6,71 +6,28 @@ public class AddTwoNumbers {
     private static final int TEN = 10;
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode(-1);
+        if (l1 == null && l2 == null) {
+            return null; // Both lists empty
+        }
+
+        ListNode result = new ListNode(0); // No dummy value needed
         ListNode current = result;
-        ListNode tmp;
         boolean carry = false;
-        int val;
 
-        while (l1 != null && l2 != null) {
-            val = l1.val + l2.val;
-            if (carry) {
-                val += 1;
-            }
+        while (l1 != null || l2 != null || carry) {
+            int val = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + (carry ? 1 : 0);
 
-            if (val >= TEN) {
-                carry = true;
-                val %= TEN;
-            } else {
-                carry = false;
-            }
+            carry = val >= TEN;
+            val %= TEN;
 
-            tmp = new ListNode(val);
-            current.next = tmp;
-            current = tmp;
+            ListNode newNode = new ListNode(val);
+            current.next = newNode;
+            current = newNode;
 
-            l1 = l1.next;
-            l2 = l2.next;
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
         }
 
-        while (l1 != null) {
-            val = l1.val;
-            if (carry) {
-                val += 1;
-            }
-            if (val >= TEN) {
-                carry = true;
-                val %= TEN;
-            } else {
-                carry = false;
-            }
-            tmp = new ListNode(val);
-            current.next = tmp;
-            current = tmp;
-            l1 = l1.next;
-        }
-
-        while (l2 != null) {
-            val = l2.val;
-            if (carry) {
-                val += 1;
-            }
-            if (val >= TEN) {
-                carry = true;
-                val %= TEN;
-            } else {
-                carry = false;
-            }
-            tmp = new ListNode(val);
-            current.next = tmp;
-            current = tmp;
-            l2 = l2.next;
-        }
-
-        if (carry) {
-            current.next = new ListNode(1);
-        }
-
-        return result.next;
+        return result;
     }
 }
