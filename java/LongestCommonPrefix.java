4cf0e4f7e27c205a664acd707fcf9be7a7176dee
Samuel Huang <samhuang91@gmail.com>
2024-11-08 08:06:03 -0800

    Update LongestCommonPrefix.java
    Add comments and use a StringBuilder

--- java/LongestCommonPrefix.java
+++ java/LongestCommonPrefix.java
@@ -1,35 +1,41 @@
 // https://leetcode.com/problems/longest-common-prefix/
 
 public class LongestCommonPrefix {
+    /**
+     * We first check if the array is empty or has only one element.
+     * If that's not the case, we find the minimum length of all
+     * the strings. We then iterate through the strings and compare
+     * the characters at the same index. If they're the same, we
+     * append the character to the prefix. If they're not, we return
+     * the prefix. If we reach the end of the loop, we return the
+     * prefix.
+     */
     public String longestCommonPrefix(String[] strs) {
         if (strs.length == 0) {
             return "";
-        } else if (strs.length == 1) {
+        }
+        if (strs.length == 1) {
             return strs[0];
         }
 
-        int minIndex = strs[0].length();
-        int maxMatch = 0;
+        int minLength = strs[0].length();
+        StringBuilder prefix = new StringBuilder();
 
         for (int i = 1; i < strs.length; i++) {
-            minIndex = Math.min(minIndex, strs[i].length());
+            minLength = Math.min(minLength, strs[i].length());
         }
 
-        while (maxMatch < minIndex) {
+        for (int i = 0; i < minLength; i++) {
             boolean isBadMatch = false;
-            char currentChar = strs[0].charAt(maxMatch);
-            for (int i = 1; i < strs.length; i++) {
-                if (currentChar != strs[i].charAt(maxMatch)) {
-                    isBadMatch = true;
-                    break;
+            char currentChar = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (currentChar != strs[j].charAt(i)) {
+                    return prefix.toString();
                 }
             }
-            if (isBadMatch) {
-                break;
-            }
-            maxMatch += 1;
+            prefix.append(currentChar);
         }
 
-        return strs[0].substring(0, maxMatch);
+        return prefix.toString();
     }
 }
