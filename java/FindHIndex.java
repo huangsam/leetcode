a732189f5bb47a401cf5e0b0c554cc1556dbf0d5
Samuel Huang <samhuang91@gmail.com>
2024-11-12 00:12:02 -0800

    Implement FindHIndex.java

---
+++ java/FindHIndex.java
@@ -0,0 +1,21 @@
+public class FindHIndex {
+    public int hIndex(int[] citations) {
+        // Values of h-index are [0, citations.length]
+        int[] frequency = new int[citations.length + 1];
+        for (int paper : citations) {
+            // We do not need precise frequencies after the last paper
+            int index = Math.min(citations.length, paper);
+            frequency[index]++;
+        }
+
+        // Start from right to left for finding maximum index faster
+        int sum = 0;
+        for (int i = frequency.length - 1; i > 0; i--) {
+            sum += frequency[i];
+            if (i <= sum) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
