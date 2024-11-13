06d64e26a14b7f14590a7ed14f252f7df3ef3385
Samuel Huang <samhuang91@gmail.com>
2024-11-13 08:54:37 -0800

    Create GasStation.java

---
+++ java/GasStation.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/gas-station/
+
+public class GasStation {
+    /**
+     * Return starting point for completing a circuit. Note
+     * that the current gas and differences help us with determining
+     * that start point more easily.
+     *
+     * <pre>
+     * gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+     * sum(gas) = 15, sum(cost) = 15
+     * diff = [-2,-2,-2,3,3]
+     *
+     * gas = [2,3,4], cost = [3,4,3]
+     * sum(gas) = 9, sum(cost) = 10
+     * diff = [-1,-1,1]
+     * </pre>
+     */
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalDifference = 0;
+        int currentGas = 0;
+        int result = 0;
+        for (int i = 0; i < gas.length; i++) {
+            int difference = gas[i] - cost[i];
+            totalDifference += difference;
+            currentGas += difference;
+            if (currentGas < 0) {
+                currentGas = 0; // Reset currentGas gas
+                result = i + 1; // Look for next starting point
+            }
+        }
+        return (totalDifference < 0) ? -1 : result;
+    }
+}
