cae7da272d701d35fe43297dfb1110ce2fdded8f
Samuel Huang <samhuang91@gmail.com>
2025-06-18 18:11:29 -0700

    Change isValidSudoku to use bitmasks

--- python/isValidSudoku.py
+++ python/isValidSudoku.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/valid-sudoku/
 
-from typing import Iterable, List
+from typing import List
 
 
 class Solution:
@@ -8,40 +8,38 @@ class Solution:
         """
         - Each row must contain the digits 1-9 without repetition.
         - Each column must contain the digits 1-9 without repetition.
-        - Each of the nine 3 x 3 sub-boxes of the grid must contain
-        the digits 1-9 without repetition.
+        - Each box must contain the digits 1-9 without repetition.
 
         Solving the rows and columns should be easy.
-        Solving the 3x3 boxes might require some more work.
-
-        Note that first list is rows. Second list is columns.
+        Solving the 3x3 boxes requires additional logic to get the index.
 
         We can skip validating characters which are dots.
         """
-        # Check board rows and columns
-        for i in range(9):
-            row = (board[i][c] for c in range(9))
-            col = (board[r][i] for r in range(9))
-            if not self.hasValidEntries(row):
-                return False
-            elif not self.hasValidEntries(col):
-                return False
+        row_masks = [0] * 9
+        col_masks = [0] * 9
+        box_masks = [0] * 9
 
-        # Check board grids
-        for i in range(3):
-            for j in range(3):
-                grid = (board[i * 3 + x][j * 3 + y] for x in range(3) for y in range(3))
-                if not self.hasValidEntries(grid):
+        # Row-major traversal
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                val = int(board[r][c])
+
+                # Check rows
+                if (row_masks[r] >> val) & 1:
                     return False
+                row_masks[r] |= 1 << val
 
-        return True
+                # Check columns
+                if (col_masks[c] >> val) & 1:
+                    return False
+                col_masks[c] |= 1 << val
 
-    def hasValidEntries(self, entries: Iterable[str]) -> bool:
-        seen = set()
-        for cell in entries:
-            if cell == ".":
-                continue
-            if cell in seen:
-                return False
-            seen.add(cell)
+                # Check boxes
+                b = (r // 3) * 3 + (c // 3)
+                if (box_masks[b] >> val) & 1:
+                    return False
+                box_masks[b] |= 1 << val
+
         return True
