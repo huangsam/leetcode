2121ee48b06bf623f62ba57544ae1341058ea328
Samuel Huang <samhuang91@gmail.com>
2025-06-27 06:43:49 -0700

    Shorten the isPowerOfFour logic

--- python/isPowerOfFour.py
+++ python/isPowerOfFour.py
@@ -5,11 +5,9 @@ class Solution:
     def isPowerOfFour(self, num: int) -> bool:
         if num <= 0:
             return False
-        if num == 1:
-            return True
 
         # Fill in powers of four in binary up to 32 bits
-        binary_mask = "0b" + "00101010101010101010101010101010"[::-1]
+        binary_mask = 0b01010101010101010101010101010101
 
         # Check if num is a power of two and if it matches the binary mask for powers of four
-        return (num & (num - 1)) == 0 and (num & int(binary_mask, 2)) == num
+        return (num & (num - 1)) == 0 and (num & binary_mask) == num
