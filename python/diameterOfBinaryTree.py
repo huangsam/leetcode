37a37f99fd10f60717ce902169db5bfb150e7e6a
Samuel Huang <samhuang91@gmail.com>
2025-06-27 06:03:38 -0700

    Adjust ordering of diameter solution

--- python/diameterOfBinaryTree.py
+++ python/diameterOfBinaryTree.py
@@ -4,13 +4,6 @@ from container.binary_tree import TreeNode
 
 
 class Solution:
-    def maxDepth(self, root: TreeNode):
-        if root is None:
-            return 0
-        left_depth = self.maxDepth(root.left) + 1
-        right_depth = self.maxDepth(root.right) + 1
-        return max(left_depth, right_depth)
-
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
         if root is None:
             return 0
@@ -19,3 +12,10 @@ class Solution:
         left_diameter = self.diameterOfBinaryTree(root.left)
         right_diameter = self.diameterOfBinaryTree(root.right)
         return max(left_depth + right_depth, left_diameter, right_diameter)
+
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        left_depth = self.maxDepth(root.left) + 1
+        right_depth = self.maxDepth(root.right) + 1
+        return max(left_depth, right_depth)
