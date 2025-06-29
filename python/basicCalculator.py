e0b7ad94668ab8e0ba29ef7914eb75e8af249399
Samuel Huang <samhuang91@gmail.com>
2025-06-29 08:59:52 -0700

    Update calculator with 3.11 syntax

--- python/basicCalculator.py
+++ python/basicCalculator.py
@@ -7,27 +7,37 @@ class Solution:
     def calculate(self, s: str) -> int:
         if not s:
             return 0
+
         stack: List[int] = []
         num = 0
         op = "+"
-        slen = len(s)
-        for i in range(slen):
-            if s[i].isdigit():
-                num = num * 10 + int(s[i])
-            if s[i] in ("+", "-", "/", "*") or i == len(s) - 1:
-                if op == "+":
-                    stack.append(num)
-                elif op == "-":
-                    stack.append(-num)
-                elif op == "*":
-                    tmp = stack.pop()
-                    stack.append(tmp * num)
-                else:
-                    tmp = stack.pop()
-                    if tmp // num < 0 and tmp % num != 0:  # round negval down
-                        stack.append(tmp // num + 1)
-                    else:
-                        stack.append(tmp // num)
+
+        for i, ch in enumerate(s):
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+
+            # Check if it's an operator or the end of the string
+            # The 'or' condition acts as a trigger to process the number and current operator
+            if ch in ("+", "-", "/", "*") or i == len(s) - 1:
+                match op:
+                    case '+':
+                        stack.append(num)
+                    case '-':
+                        stack.append(-num)
+                    case '*':
+                        tmp = stack.pop()
+                        stack.append(tmp * num)
+                    case '/':
+                        tmp = stack.pop()
+                        if tmp // num < 0 and tmp % num != 0:
+                            stack.append(tmp // num + 1)
+                        else:
+                            stack.append(tmp // num)
+
+                # Reset num and update the operator
                 num = 0
-                op = s[i]
+
+                # Update the operator to the current character
+                op = ch
+
         return sum(stack)
