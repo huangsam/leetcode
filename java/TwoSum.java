26d22f525274d52bb19a110e816372d4f35e8424
Samuel Huang <samhuang91@gmail.com>
2025-06-29 01:52:46 -0700

    Add JavaDoc to TwoSum.java

--- java/TwoSum.java
+++ java/TwoSum.java
@@ -3,6 +3,13 @@
 import java.util.HashMap;
 
 public class TwoSum {
+    /**
+     * We create a mapping of the numbers we have seen so far. While
+     * we do this, we also check if the complement of the current
+     * number (i.e., target - nums[i]) exists in the map. If it
+     * does, we return the indices of the two numbers that add up to
+     * the target.
+     */
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> seenValues = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
