ab9745f7467b546a3c448792a1987d8cffd9ac00
Samuel Huang <samhuang91@gmail.com>
2025-06-29 02:50:46 -0700

    Add JavaDoc to IsValidBST.java

--- java/IsValidBST.java
+++ java/IsValidBST.java
@@ -5,6 +5,11 @@ import container.TreeNode;
 public class IsValidBST {
     private TreeNode last = null;
 
+    /**
+     * We process the answer recursively with in-order traversal. The idea is to
+     * look for a previous entry if it exists and see if that entry is less than,
+     * or equal to, the current entry.
+     */
     public boolean isValidBST(TreeNode root) {
         if (root == null) {
             return true;
