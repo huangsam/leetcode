093c1b5d78bbc0d4e56a0d47d67761057959aaff
Samuel Huang <samhuang91@gmail.com>
2025-06-30 09:02:18 -0700

    Adjust comments in path solutions

--- python/uniquePaths.py
+++ python/uniquePaths.py
@@ -3,22 +3,22 @@
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # initialize memo array
+        # Initialize memo array
         memo = [[0] * n for _ in range(m)]
 
-        # go through each row, m-1 --> 0
+        # Go through each row, m-1 --> 0
         for r in range(m - 1, -1, -1):
-            # go through each col, n-1 --> 0
+            # Go through each col, n-1 --> 0
             for c in range(n - 1, -1, -1):
-                # base case -> at the end
+                # Base case -> at the end
                 if r == m - 1 and c == n - 1:
                     memo[r][c] = 1
-                # can go down
+                # Can go down
                 if r + 1 < m:
                     memo[r][c] += memo[r + 1][c]
-                # can go right
+                # Can go right
                 if c + 1 < n:
                     memo[r][c] += memo[r][c + 1]
 
-        # solve original problem --> starting point = (0,0)
+        # Solve original problem --> starting point = (0,0)
         return memo[0][0]
