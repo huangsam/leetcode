bfe35b01c84b6d4cfa3cc1adca90c99327184626
Samuel Huang <samhuang91@gmail.com>
2025-07-01 10:35:37 -0700

    Add comments to invertTree.py

--- python/invertTree.py
+++ python/invertTree.py
@@ -9,8 +9,13 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if root is None:
             return None
+
+        # Swap the left and right children
         if root.left or root.right:
             root.left, root.right = root.right, root.left
+
+        # Recursively invert the left and right subtrees
         root.left = self.invertTree(root.left)
         root.right = self.invertTree(root.right)
+
         return root
