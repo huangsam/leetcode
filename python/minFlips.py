10d0903a3571010540d822db01c04f75593c8837
Samuel Huang <samhuang91@gmail.com>
2025-06-30 23:47:21 -0700

    Leverage one-pass for minFlips.py

--- python/minFlips.py
+++ python/minFlips.py
@@ -3,26 +3,22 @@
 
 class Solution:
     def minFlips(self, a: int, b: int, c: int) -> int:
-        # Calculate longest length
-        longest = 0
-        a_tmp, b_tmp, c_tmp = a, b, c
-        while a_tmp | b_tmp | c_tmp:
-            a_tmp = a_tmp >> 1
-            b_tmp = b_tmp >> 1
-            c_tmp = c_tmp >> 1
-            longest += 1
-
-        # Calculate the number of moves from [0, longest)
         result = 0
-        for shift in range(longest):
-            a_one = (a >> shift) & 1
-            b_one = (b >> shift) & 1
-            c_one = (c >> shift) & 1
-            if a_one | b_one == c_one:
-                continue
-            if c_one == 1:
-                result += 1
-            else:
-                result += 1 + (a_one & b_one)
+        while a > 0 or b > 0 or c > 0:
+            a_bit = a & 1
+            b_bit = b & 1
+            c_bit = c & 1
+
+            if a_bit | b_bit != c_bit:
+                # Case 1: c_bit is 1, but (a_bit | b_bit) is not 1
+                if c_bit == 1:
+                    result += 1
+                # Case 2: c_bit is 0, but (a_bit | b_bit) is not 0
+                else:
+                    result += 1 + (a_bit & b_bit)
+
+            a >>= 1
+            b >>= 1
+            c >>= 1
 
         return result
