a0b6358998408b5668075a0895afc457e0de68a2
Samuel Huang <samhuang91@gmail.com>
2025-06-30 22:48:39 -0700

    Add comments to findDisappearedNumbers.py

--- python/findDisappearedNumbers.py
+++ python/findDisappearedNumbers.py
@@ -6,13 +6,18 @@ from typing import List
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         result = []
+
+        # Use the index of the array to mark the presence of numbers
         n_len = len(nums)
         for i in range(n_len):
             num = abs(nums[i])
             if nums[num - 1] > 0:
                 nums[num - 1] *= -1
+
+        # Collect the indices of the numbers that were not marked
         n_len = len(nums)
         for i in range(n_len):
             if nums[i] > 0:
                 result.append(i + 1)
+
         return result
