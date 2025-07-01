2edd05f11e0713aea4fdca48703508d8c8cf1a60
Samuel Huang <samhuang91@gmail.com>
2025-07-01 00:16:56 -0700

    Add comments to threeSum.py

--- python/threeSum.py
+++ python/threeSum.py
@@ -8,10 +8,14 @@ class Solution:
         solution = set()
         nlen = len(nums)
         sums = {}
+
+        # Do initial caching
         for i in range(nlen):
             for j in range(nlen):
                 if i != j:
                     sums[(i, j)] = nums[i] + nums[j]
+
+        # Run an O(n^2) loop to find pairs that sum to -nums[k]
         for k in range(nlen):
             for key, val in sums.items():
                 if nums[k] == -val:
@@ -19,4 +23,5 @@ class Solution:
                     if i != k and j != k:
                         res = tuple(sorted([nums[i], nums[j], nums[k]]))
                         solution.add(res)
+
         return sorted(list(res) for res in solution)
