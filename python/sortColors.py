cd39e8ac0045e416355ac00e679f05e12d549199
Samuel Huang <samhuang91@gmail.com>
2025-07-01 00:15:12 -0700

    Add comments to sortColors.py

--- python/sortColors.py
+++ python/sortColors.py
@@ -7,12 +7,15 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         lo, mid, hi = 0, 0, len(nums) - 1
         while mid <= hi:
+            # If the current element is 0, swap it with the element at lo
             if nums[mid] == 0:
                 nums[lo], nums[mid] = nums[mid], nums[lo]
                 lo += 1
                 mid += 1
+            # If the current element is 1, just move to the next element
             elif nums[mid] == 1:
                 mid += 1
+            # If the current element is 2, swap it with the element at hi
             elif nums[mid] == 2:
                 nums[mid], nums[hi] = nums[hi], nums[mid]
                 hi -= 1
