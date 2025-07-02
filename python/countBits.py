e74154ec1d79c87846359aafdbd9b32056337512
Samuel Huang <samhuang91@gmail.com>
2025-07-01 18:05:35 -0700

    Remove redundant comment in countBits

--- python/countBits.py
+++ python/countBits.py
@@ -18,6 +18,5 @@ class Solution:
         """
         dp = [0] * (n + 1)
         for i in range(1, n + 1):
-            # Recurrence relation
             dp[i] = dp[i // 2] + (i % 2)
         return dp
