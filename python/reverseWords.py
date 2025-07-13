af30684a7b7209c1502db433c62b5ea536498533
Samuel Huang <samhuang91@gmail.com>
2025-07-13 10:08:45 -0700

    Update repository to use uv (#6)
    * Update repository to use uv
    
    * Add comments to reverseWords.py

--- python/reverseWords.py
+++ python/reverseWords.py
@@ -4,6 +4,11 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         reversed_words = []
+
+        # Process all tokens in reverse order
         for rev_word in s.strip()[::-1].split():
+            # Each token can be reversed to get the word
             reversed_words.append(rev_word[::-1])
+
+        # Join all words as a single string
         return " ".join(reversed_words)
