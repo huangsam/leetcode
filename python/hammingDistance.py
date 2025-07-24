3f1a57f333af578bc793ddc2d136f04d5a90c273
Samuel Huang <samhuang91@gmail.com>
2025-07-24 10:11:23 -0700

    Use divmod for hammingDistance.py

--- python/hammingDistance.py
+++ python/hammingDistance.py
@@ -4,14 +4,10 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
         distance = 0
-
-        # Calculate the XOR of x and y
         xor_result = x ^ y
 
-        # Count the number of '1's in the binary output
         while xor_result > 0:
-            if xor_result & 1:
-                distance += 1
-            xor_result >>= 1
+            xor_result, remainder = divmod(xor_result, 2)
+            distance += remainder
 
         return distance
