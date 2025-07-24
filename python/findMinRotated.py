055d2cc28af45739b16d4df605a63d0b13b1fc78
Samuel Huang <samhuang91@gmail.com>
2025-07-24 00:51:52 -0700

    Consolidate to one approach

--- python/findMinRotated.py
+++ python/findMinRotated.py
@@ -6,23 +6,10 @@ from typing import List
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         lo, hi = 0, len(nums) - 1
-        mid = (lo + hi) // 2
-        if len(nums) <= 2:
-            return min(nums)
-        mid_val = nums[mid]
-        last_val = nums[hi]
-        if mid_val > last_val:
-            return self.findMin(nums[mid : hi + 1])
-        return self.findMin(nums[lo : mid + 1])
-
-    def findMinIterative(self, nums: List[int]) -> int:
-        lo, hi = 0, len(nums) - 1
-        while hi - lo > 2:
-            mid = (lo + hi) // 2
-            mid_val = nums[mid]
-            last_val = nums[hi]
-            if mid_val > last_val:
-                lo = mid
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if nums[mid] > nums[hi]:
+                lo = mid + 1
             else:
                 hi = mid
-        return min(nums[lo : hi + 1])
+        return nums[lo]
