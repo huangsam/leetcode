cc7e45636024d86532bfa22bd5d6e6b0b52220e3
Samuel Huang <samhuang91@gmail.com>
2025-07-24 01:06:34 -0700

    Optimize findMedianSortedArrays.py

--- python/findMedianSortedArrays.py
+++ python/findMedianSortedArrays.py
@@ -3,31 +3,44 @@
 from typing import List
 
 
-# TODO: Make this solution more efficient
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        arr = self._merge(nums1, nums2)
-        alen = len(arr)
-        middle = ((1 + len(arr)) // 2) - 1
-        if alen % 2 == 0:
-            left, right = middle, middle + 1
-            answer = float(arr[left] + arr[right]) / 2.0
-        else:
-            answer = arr[middle]
-        return answer
+        # Ensure nums1 is the shorter array for efficient binary search
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
 
-    def _merge(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nlen1 = len(nums1)
-        nlen2 = len(nums2)
-        c1, c2 = 0, 0
-        arr = []
-        while c1 < nlen1 and c2 < nlen2:
-            if nums1[c1] <= nums2[c2]:
-                arr.append(nums1[c1])
-                c1 += 1
-            else:
-                arr.append(nums2[c2])
-                c2 += 1
-        arr += nums1[c1:]
-        arr += nums2[c2:]
-        return arr
+        m, n = len(nums1), len(nums2)
+        low, high = 0, m  # Binary search on nums1's partition
+
+        while low <= high:
+            partitionX = (low + high) // 2
+            partitionY = (m + n + 1) // 2 - partitionX
+
+            # Determine elements around partitionX
+            # If partitionX is 0, nothing on left, so maxLeftX is -infinity
+            # If partitionX is m, nothing on right, so minRightX is +infinity
+            maxLeftX = float("-inf") if partitionX == 0 else nums1[partitionX - 1]
+            minRightX = float("inf") if partitionX == m else nums1[partitionX]
+
+            # Determine elements around partitionY
+            maxLeftY = float("-inf") if partitionY == 0 else nums2[partitionY - 1]
+            minRightY = float("inf") if partitionY == n else nums2[partitionY]
+
+            # Check if partitions are correct
+            if maxLeftX <= minRightY and maxLeftY <= minRightX:
+                # Found the correct partition
+                if (m + n) % 2 == 0:
+                    # Even number of elements
+                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0
+                else:
+                    # Odd number of elements
+                    return float(max(maxLeftX, maxLeftY))
+            elif maxLeftX > minRightY:
+                # partitionX is too far right, need to move left
+                high = partitionX - 1
+            else:  # maxLeftY > minRightX
+                # partitionX is too far left, need to move right
+                low = partitionX + 1
+
+        # This part should ideally not be reached if inputs are valid
+        return 0.0  # Or raise an error
