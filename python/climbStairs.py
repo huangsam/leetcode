9f3bb382fc83d600ef756902f6f408ddddbbaefd
Samuel Huang <samhuang91@gmail.com>
2025-07-24 00:44:55 -0700

    Implement climbStairs.py

---
+++ python/climbStairs.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 1:
+            return 1
+
+        # The solution is very similar to the bottoms-up DP approach
+        # for fibonacci. To save on space, we use two variables
+        # instead of a Python list
+        f1, f2 = 1, 1
+        for _ in range(2, n + 1):
+            current = f1 + f2
+            f1 = f2
+            f2 = current
+
+        return f2
