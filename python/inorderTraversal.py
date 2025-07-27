64536fcf240650b0951c7807376ac2ad847000b9
Samuel Huang <samhuang91@gmail.com>
2025-07-27 00:29:49 -0700

    Add comments to inorderTraversal.py

--- python/inorderTraversal.py
+++ python/inorderTraversal.py
@@ -9,20 +9,30 @@ class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
         if root is None:
             return []
+
+        # Classic recursive solution
         result = self.inorderTraversal(root.left)
         result.append(root.val)
         result.extend(self.inorderTraversal(root.right))
+
         return result
 
     def inorderTraversalIterative(self, root: TreeNode) -> List[int]:
         visited: List[int] = []
         c = root
         s = []
+
         while c is not None or len(s) > 0:
+            # Keep going left
             while c is not None:
                 s.append(c)
                 c = c.left
+
+            # Visit current node
             c = s.pop()
             visited.append(c.val)
+
+            # Go to next node to the right
             c = c.right
+
         return visited
