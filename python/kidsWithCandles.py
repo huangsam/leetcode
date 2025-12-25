3159fdb6e62e91205a191d9e7cb0391218b047d1
Samuel Huang <samhuang91@gmail.com>
2025-12-24 16:41:15 -0800

    Implement kidsWithCandles.py

---
+++ python/kidsWithCandles.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        """
+        Start by finding the max possible value. Then create a boolean list
+        of len(candies) where you literally go through a second pass. Can I be
+        more efficient simply by relying the i and i-1? No I don't think so
+        because that would only give you the local maxima, not the absolute
+        maxima. You can only find the absolute maxima in O(N).
+        """
+        max_count_for_kid = max(candies)
+
+        result = [False] * len(candies)
+        for index, current_count in enumerate(candies):
+            if current_count + extraCandies >= max_count_for_kid:
+                result[index] = True
+
+        return result
