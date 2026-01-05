5d6b63d4a0e9123fda71c4514157bf8d0de7eb39
Samuel Huang <samhuang91@gmail.com>
2026-01-05 08:55:14 -0800

    Resolve randomset spacing

--- java/RandomizedSet.java
+++ java/RandomizedSet.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-    /**
+/**
  * Your RandomizedSet object will be instantiated and called as such:
  * RandomizedSet obj = new RandomizedSet();
  * boolean param_1 = obj.insert(val);
