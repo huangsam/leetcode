9289a529ba6283169856c0dbbf9e0c5b992dc9ee
Samuel Huang <samhuang91@gmail.com>
2026-01-18 07:47:35 -0800

    Refactor productExceptSelf for readability

--- python/productExceptSelf.py
+++ python/productExceptSelf.py
@@ -8,17 +8,16 @@ class Solution:
         """
         Return an array where each element is the product of all others.
 
-
         For example [1,2,3,4] ->
 
-        Multiply answer[i] by prefix product, before nums[i].
+        Multiply answer[i] by prefix product, before nums[i] from the left.
 
         prefix @ nums[0] -> $1
         prefix @ nums[1] -> $1 * 1
         prefix @ nums[2] -> $1 * 1 * 2
         prefix @ nums[3] -> $1 * 1 * 2 * 3
 
-        Multiply answer[i] by suffix product, before nums[i].
+        Multiply answer[i] by suffix product, before nums[i] from the right.
 
         suffix @ nums[3] -> $1
         suffix @ nums[2] -> $1 * 4
@@ -29,19 +28,18 @@ class Solution:
         - Time: O(n)
         - Space: O(1)
         """
-        answer = [1] * len(nums)
-        left_val, right_val = 1, 1
-        for left_idx in range(len(nums)):
-            right_idx = len(nums) - left_idx - 1
-
+        n = len(nums)
+        answer = [1] * n
+        left_prefix, right_suffix = 1, 1
+        for left_idx in range(n):
+            right_idx = n - left_idx - 1
             left_num, right_num = nums[left_idx], nums[right_idx]
 
-            # Prefix product before nums[left_idx]
-            answer[left_idx] *= left_val
+            # Update answer with prefix and suffix products
+            answer[left_idx] *= left_prefix
+            answer[right_idx] *= right_suffix
 
-            # Suffix product before nums[right_idx]
-            answer[right_idx] *= right_val
-
-            left_val *= left_num
-            right_val *= right_num
+            # Update prefix and suffix products
+            left_prefix *= left_num
+            right_suffix *= right_num
         return answer
