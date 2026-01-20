6c7a402e5441a1ba121d4865c7f69200cd6ba8d2
Samuel Huang <samhuang91@gmail.com>
2026-01-20 10:14:50 -0800

    Optimize groupAnagrams.py

--- python/groupAnagrams.py
+++ python/groupAnagrams.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/group-anagrams/
 
-from collections import defaultdict
+from collections import Counter, defaultdict
 from typing import DefaultDict, List
 
 
@@ -11,11 +11,8 @@ class Solution:
 
         Assume that anagram is simply a map of alpha counts.
 
-        Then we will generate a hash based on the frequency count of
-        all 26 letters in the alphabet.
-
-        Then each of the strings with the exact same hash will fall
-        under the same key.
+        Then we can create a hash key from the character frequency count.
+        Each unique hash key will map to a list of strings that are anagrams.
 
         Lastly, we iterate through the values of the dictionary, returning
         it as a list of lists.
@@ -24,16 +21,10 @@ class Solution:
         - Time: O(n * k)
         - Space: O(n * k)
         """
-        strings_by_hash: DefaultDict[int, list] = defaultdict(list)
+        strings_by_hash: DefaultDict[tuple, list] = defaultdict(list)
         for content in strs:
-            strings_by_hash[self._getHash(content)].append(content)
+            strings_by_hash[self._getKey(content)].append(content)
         return list(strings_by_hash.values())
 
-    def _getHash(self, content: str) -> int:
-        freq = [0] * 26
-        for ch in content:
-            freq[ord(ch) - ord("a")] += 1
-        key_list = []
-        for idx, val in enumerate(freq):
-            key_list.append(f"{idx}-{val}")
-        return hash(tuple(key_list))
+    def _getKey(self, content: str) -> tuple:
+        return tuple(sorted(Counter(content).items()))
