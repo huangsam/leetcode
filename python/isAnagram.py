6a9a02226f2e61d6eff07992b2963bd30428cfec
Samuel Huang <samhuang91@gmail.com>
2026-01-20 10:15:00 -0800

    Optimize isAnagram.py

--- python/isAnagram.py
+++ python/isAnagram.py
@@ -1,7 +1,6 @@
 # https://leetcode.com/problems/valid-anagram/
 
-from collections import defaultdict
-from typing import DefaultDict
+from collections import Counter
 
 
 class Solution:
@@ -16,9 +15,4 @@ class Solution:
         - Time: O(n + m)
         - Space: O(n)
         """
-        s_count: DefaultDict[str, int] = defaultdict(int)
-        for ch in s:
-            s_count[ch] += 1
-        for ch in t:
-            s_count[ch] -= 1
-        return all(v == 0 for v in s_count.values())
+        return Counter(s) == Counter(t)
