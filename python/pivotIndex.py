8868044bd37088702f9e8d6c4ccc039c55f66154
Samuel Huang <samhuang91@gmail.com>
2026-02-07 04:23:40 -0800

    Implement pivotIndex.py

---
+++ python/pivotIndex.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        """
+        Find the pivot index of an array, where sum to the left of the index
+        is equal to sum to the right of the index. Return -1 otherwise.
+
+        Complexity:
+        - Time: O(n)
+        - Space: O(1)
+        """
+        total_sum = sum(nums)
+        left_sum = 0
+
+        for i, j in enumerate(nums):
+            # Exclude current element and left_sum
+            right_sum = total_sum - j - left_sum
+
+            # Check for an exact match on left and right
+            if left_sum == right_sum:
+                return i
+
+            left_sum += j
+
+        # Default answer if we cannot find anything
+        return -1
