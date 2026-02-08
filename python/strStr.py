e9060b9a859a270e50c4699d1a81a6dc618f29bd
Samuel Huang <samhuang91@gmail.com>
2026-02-08 08:06:51 -0800

    Implement strStr.py

--- python/strStr.py
+++ python/strStr.py
@@ -6,47 +6,25 @@ class Solution:
         """
         Find the first occurrence of a substring in a string.
 
-        Start from the beginning. Whenever there is a match of needle in
-        haystack, then we start iterating and keep track of index. If there
-        is a full match, then we store the minimum result, as we are looking
-        for the first occurrence of needle. If we end up with no match,
-        we will return -1 instead of a non-negative integer.
+        To make this more optimized, we can either implement Rabin-Karp or
+        simply leverage haystack.find(needle) for linear performance.
 
         Complexity:
         - Time: O(n * m)
-        - Space: O(1)
+        - Space: O(m)
         """
-        # End result that we're looking for
-        match_at = -1
+        m, n = len(needle), len(haystack)
 
-        # The size of the full match required to get needle
-        needle_size = len(needle)
+        # Escape if needle is bigger
+        if m > n:
+            return -1
 
-        # The progress that we're making towards a needle
-        needle_prog = 0
-
-        # Initial starting point
-        ptr = 0
-
-        # Iterate through the whole haystack for a needle
-        while ptr < len(haystack):
-            # Increment progress if a match occurs
-            if haystack[ptr] == needle[needle_prog]:
-                needle_prog += 1
-
-            # Or reset progress and pointer if a match does not occur
-            else:
-                ptr = ptr - needle_prog + 1
-                needle_prog = 0
+        # Find needle in haystack
+        for left in range(n - m + 1):
+            if haystack[left] != needle[0]:
                 continue
+            if haystack[left : left + m] == needle:
+                return left
 
-            # Update pointer, otherwise loop will run forever!
-            ptr += 1
-
-            # If we found a full match, update result and reset progress
-            if needle_prog == needle_size:
-                # Note that we must handle the initial case and nth case
-                match_at = ptr - needle_prog if match_at == -1 else min(match_at, ptr - needle_prog)
-                needle_prog = 0
-
-        return match_at
+        # Fallback answer when no match is found
+        return -1
